954768003e39329bd3cf9e9cd76ea40c47b5129b
lucidor <rtlucido@hotmail.com>
2017-07-30 21:40:33 -0400

    Worked on Hand class and edited player class

--- CrazyEights/Hand.cs
+++ CrazyEights/Hand.cs
@@ -8,5 +8,33 @@ namespace CrazyEights
 {
     class Hand
     {
+        private Deck _deck;
+        private List<Card> _hand;
+
+
+        public Hand(List<Card> hand)
+        {
+            _hand = hand;
+
+        }
+
+
+        public Card GetCard(byte num)
+        {
+            return _hand[num];
+
+        }
+
+        public void RemoveCard(byte num)
+        {
+            _hand.RemoveAt(num);
+
+        }
+        
+        public void AddCard(Card card)
+        {
+            _hand.Add(card);
+        }
+
     }
 }
