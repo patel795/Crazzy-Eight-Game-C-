cf79cc1810d370e3e563ceb588a87b68f1388d76
Deep Patel <patel795@sheridancollege.ca>
2017-08-05 15:10:52 -0400

    CardCollection methods implemented

--- CrazyEights/CardCollection.cs
+++ CrazyEights/CardCollection.cs
@@ -6,8 +6,41 @@ using System.Threading.Tasks;
 
 namespace CrazyEights
 {
-    class CardCollection
+    public class CardCollection
     {
+        private string _label;
 
+        private List<Card> _cardList;
+
+        public CardCollection(string label)
+        {
+            this._label = label;
+            this._cardList = new List<Card>();
+        }
+
+        private void AddCard(Card card)
+        {
+            this._cardList.Add(card);
+        }
+
+        private void RemoveCard(Card card)
+        {
+            this._cardList.Remove(card);
+        }
+
+        private int Size()
+        {
+            return _cardList.Count;
+        }
+
+        private bool Empty()
+        {
+            return _cardList.Count == 0;
+        }
+
+        private void Deal()
+        {
+
+        }
     }
 }
